2a22586086b6aa08e6aebc8a449a80c8d3925f8c
skinreich11 <stavkin11@gmail.com>
2024-04-30 18:53:11 -0400

    Changing app look

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <div>
     <header className={styles.header}>
-      <h1 >Movie Recommender</h1>
-      {/* Search bar or other elements */}
+    <h1>Movie Recommender</h1>
+      <div className={styles.right}>{globalThis.userName === null || globalThis.userName === undefined ?
+      <Link to="/Login">Login/Register</Link> : <h3>Hello, {globalThis.userName}</h3>}</div>
     </header>
   </div>
   );
